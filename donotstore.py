aba8c23501e235da1ee8a17244f9df70d0057cea
endzzz <40990773+endzzz@users.noreply.github.com>
2019-03-30 17:47:11 +0800

    Add files via upload

--- donotstore.py
+++ donotstore.py
@@ -10,6 +10,5 @@ from sklearn.linear_model import SGDRegressor
 import os
 m = np.zeros((2,3,4))
 z = np.ones((3,4))
-m1 = np.append(m,z)
-m2 = np.dstack((m,z))
-print(m2)
+m1 = np.append(m,z) #result = [0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 0. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1. 1.]
+m2 = np.dstack((m,z))# result = ValueError: all the input array dimensions except for the concatenation axis must match exactly
