5849d16ed28fb4fa533d15e4cb6140262d519757
endzzz <40990773+endzzz@users.noreply.github.com>
2019-06-22 23:34:34 +0800

    Add files via upload
    try different method

--- build_array.py
+++ build_array.py
@@ -10,7 +10,7 @@ from spectral import *
 import csv
     # need to find the path first
     # locate the target folder 
-folder = 'D:/HOMEWORK/rslab/IOCS_2019/ENVI_daily_data/decay' # 要改
+folder = 'D:/HOMEWORK/rslab/IOCS_2019/ENVI_daily_data/decay' # change this path if u want.
 date = os.listdir(folder)
 #### find the number of row and colume
 standard_img = envi.open('D:/HOMEWORK/rslab/IOCS_2019/ENVI_daily_data/SS/20150803/20150803_0_SS.hdr')
@@ -66,8 +66,14 @@ def regression_form(t,a,b,c):
     with np.errstate(divide = 'ignore'):
         s = np.float64(a*np.exp(-1*b*t)+c)
     return s
+def regression_form2(t,b,c):
+    # this form contain only 2 variable
+    with np.errstate(divide = 'ignore'):
+        s = np.float64(10*np.exp(-1*b*t)+c)
+    return s
 
 def do_regression(img_matrix, t_martix,row,col):
+    # start from the point right after the max point.
     a_matrix = np.zeros((row, col))
     b_matrix = np.zeros((row, col))
     c_matrix = np.zeros((row, col))
@@ -84,15 +90,14 @@ def do_regression(img_matrix, t_martix,row,col):
             ## find the max value
             t_use = [] # matrix used to regression
             ss_use = []
-            if len(t_temp) >1:
-                for n in range(ss_temp.index(max(ss_temp)), len(ss_temp)):
+            if len(t_temp) >5:
+                for n in range(ss_temp.index(max(ss_temp))+1, len(ss_temp)):
                     t_use.append(t_temp[n])
                     ss_use.append(ss_temp[n])
                             # regression: concentration = a*e^(-b*t)+c
-                    difference = max(ss_use)-min(ss_use)
             if len(t_use)>5 :
                 try:
-                    [popt, pcov] = curve_fit(regression_form, t_use, ss_use,maxfev = 1000,p0=[13,0.011,1])
+                    [popt, pcov] = curve_fit(regression_form, t_use, ss_use,maxfev = 50,p0=[5,0.011,1],bounds=(0,[15,20,10]))
                     a_matrix[i][j] = popt[0]
                     b_matrix[i][j] = popt[1]
                     c_matrix[i][j] = popt[2]
@@ -101,14 +106,17 @@ def do_regression(img_matrix, t_martix,row,col):
                     b_matrix[i][j] = 0
                     c_matrix[i][j] = 0
                     continue
+    envi.save_image('b.hdr', b_matrix)
+    envi.save_image('a.hdr', a_matrix)
+    envi.save_image('c.hdr', c_matrix)
 
             # print(t_temp)
-            ############write a csv file to help us define the initial guess
-            if i == 100 and j == 100:
-                with open('regression.csv','w') as csvfile:
-                    save = csv.writer(csvfile, delimiter = ',')
-                    save.writerow(t_use)
-                    save.writerow(ss_use)            
+            ############ write a csv file to help us define the initial guess
+            # if i == 100 and j == 100:
+            #     with open('regression.csv','w') as csvfile:
+            #         save = csv.writer(csvfile, delimiter = ',')
+            #         save.writerow(t_use)
+            #         save.writerow(ss_use)            
 
 
     return a_matrix, b_matrix, c_matrix
@@ -132,7 +140,7 @@ def do_regression2(img_matrix, t_martix,row,col):
 
             if len(t_temp)>5 :
                 try:
-                    [popt, pcov] = curve_fit(regression_form, t_temp, ss_temp,maxfev = 1000,p0=[13,0.011,1])
+                    [popt, pcov] = curve_fit(regression_form, t_temp, ss_temp,maxfev = 1000,p0=[13,0.011,1],bounds=(0,[np.inf,np.inf,10]))
                     a_matrix[i][j] = popt[0]
                     b_matrix[i][j] = popt[1]
                     c_matrix[i][j] = popt[2]
@@ -147,6 +155,91 @@ def do_regression2(img_matrix, t_martix,row,col):
                     continue
 
     return a_matrix, b_matrix, c_matrix
+
+def do_regression3(img_matrix, t_martix,row,col):
+    # do regression from higher but not highest value by using threshold
+    a_matrix = np.zeros((row, col))
+    b_matrix = np.zeros((row, col))
+    c_matrix = np.zeros((row, col))
+    num =0
+    for i in range(row):
+        for j in range(col):
+            ss_temp = []
+            t_temp = [] #this array restore the hour 
+            for t in range(total_image_num):
+                if img_matrix[t][i][j] > 0:
+                    ss_temp.append(img_matrix[t][i][j]) # temporary matrix to secure the sequence of value in different time
+                    t_temp.append(t_martix[t]) # temporary matrix to secure the hours correlate to the m_temp
+            
+            
+            ## define threshold
+            n = 0
+            
+            ss_use =[]
+            t_use=[]
+            if len(ss_temp)>5:
+                threshold = 0.8*np.mean(ss_temp)
+            
+                for k in range(len(ss_temp)):
+                    if ss_temp[k]<threshold:
+                        n = n+1
+                    else:
+                        break
+
+                for k in range(n,len(ss_temp)):
+                    ss_use.append(ss_temp[k])
+                    t_use.append(t_temp[k])
+            if len(t_use)>5 :
+                try:
+                    [popt, pcov] = curve_fit(regression_form, t_use, ss_use,maxfev = 1000,p0=[13,0.011,1],bounds=([0,0,0],[np.inf,np.inf,8]))
+                    a_matrix[i][j] = popt[0]
+                    b_matrix[i][j] = popt[1]
+                    c_matrix[i][j] = popt[2]
+                    num = num+1
+                except:
+                    a_matrix[i][j] = 0
+                    b_matrix[i][j] = 0
+                    c_matrix[i][j] = 0
+                    continue
+    print('num:',num)
+    return a_matrix, b_matrix, c_matrix
+
+def do_regression4(img_matrix, t_martix,row,col):
+    # start from the point right after the max point and let a =10.
+    b_matrix = np.zeros((row, col))
+    c_matrix = np.zeros((row, col))
+    for i in range(row):
+        for j in range(col):
+
+
+            ss_temp = []
+            t_temp = [] #this array restore the hour 
+            for t in range(total_image_num):
+                if img_matrix[t][i][j] > 0:
+                    ss_temp.append(img_matrix[t][i][j]) # temporary matrix to secure the sequence of value in different time
+                    t_temp.append(t_martix[t]) # temporary matrix to secure the hours correlate to the m_temp
+            ## find the max value
+            t_use = [] # matrix used to regression
+            ss_use = []
+            if len(t_temp) >5:
+                for n in range(ss_temp.index(max(ss_temp))+1, len(ss_temp)):
+                    t_use.append(t_temp[n])
+                    ss_use.append(ss_temp[n])
+                            # regression: concentration = a*e^(-b*t)+c
+            if len(t_use)>5 :
+                try:
+                    [popt, pcov] = curve_fit(regression_form2, t_use, ss_use,maxfev = 1000,p0=[0.011,1],bounds=(0,[np.inf,10]))
+                    b_matrix[i][j] = popt[0]
+                    c_matrix[i][j] = popt[1]
+                except:
+                    b_matrix[i][j] = 0
+                    c_matrix[i][j] = 0
+                    continue
+
+    envi.save_image('b_reg4.hdr', b_matrix)
+    envi.save_image('c_reg4.hdr', c_matrix)
+
+
 #save array into envi_file:
 def save_as_envi(img_matrix):
     # let img_matrix[t][row][col] become img_matrix[row][col][t] because the third index will become the band numbers.
@@ -163,17 +256,18 @@ def save_as_envi(img_matrix):
 ########### statistic
 # image_statistic(img_matrix,row,col)
 ########### regression
-do_regression(img_matrix,t_martix,row,col)
+# do_regression(img_matrix,t_martix,row,col)
 
-result = do_regression2(img_matrix,t_martix,row,col)
-a_matrix = result[0]
-b_matrix = result[1]
-c_matrix = result[2]
-############ export data
-envi.save_image('b.hdr', b_matrix)
-envi.save_image('a.hdr', a_matrix)
-envi.save_image('c.hdr', c_matrix)
+# result = do_regression(img_matrix,t_martix,row,col)
+# a_matrix = result[0]
+# b_matrix = result[1]
+# c_matrix = result[2]
+# ############ export data
+# envi.save_image('b.hdr', b_matrix)
+# envi.save_image('a.hdr', a_matrix)
+# envi.save_image('c.hdr', c_matrix)
 # with open('t_matrix.csv','w') as csvfile:
 #     save = csv.writer(csvfile, delimiter = ',')
 #     save.writerow(t_martix)
-print(t_martix)
+
+do_regression(img_matrix,t_martix,row,col)
